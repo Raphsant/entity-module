d02074d6d18f1964d7ff67d187c1e5dd4998992c
Rafael Santana <rsantana@colum.edu>
2024-06-25 15:41:05 -0500

    Implement user management and appointment scheduling features
    This commit includes new Vue components that provide functionalities for managing user data and scheduling appointments. It also introduces a layout for the web page, supports for handling insurance providers and updates to the main.css stylesheet. These updates enhance the application's user management and scheduling capabilities.

---
+++ nuxt.config.ts
@@ -0,0 +1,66 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ["@nuxt/ui-pro"],
+  runtimeConfig: {
+    public: {
+      serverUri: "",
+    },
+  },
+  modules: [
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@nuxt/ui",
+    "@nuxtjs/fontaine",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/device",
+    "@vueuse/nuxt",
+    "@sidebase/nuxt-pdf",
+  ],
+  //@ts-ignore
+  browser: "firefox",
+  colorMode: {
+    preference: "dark",
+  },
+  googleFonts: {
+    display: "swap",
+    download: true,
+    families: {
+      "DM+Sans": [400, 500, 600, 700],
+    },
+  },
+  fontMetrics: {
+    fonts: ["DM Sans"],
+  },
+  nitro: {
+    // Nitro options
+  },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  ui: {
+    icons: ["ph", "simple-icons"],
+    //@ts-ignore
+    notifications: {
+      // Show toasts at the top right of the screen
+      position: "top-0 bottom-auto",
+    },
+  },
+  router: {
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition;
+      } else if (to.hash) {
+        return {
+          el: to.hash,
+          behavior: 'smooth',
+        };
+      } else {
+        return {top: 0};
+      }
+    }
+  }
+});
